552c09fff97e57bd0b26cce14ba2eff4a14ebe4e
Wenderson Barros <wendercesar2@gmail.com>
2021-04-04 17:12:35 -0400

    Primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+function insertTask() {
+
+    var element = document.getElementById("task");
+    var task = element.value;
+    var textTask = document.createTextNode(task);
+
+    if (task.length > 0) {
+
+        var a = document.createElement("p");
+        a.appendChild(textTask);
+
+        var b = document.createElement("input");
+        b.setAttribute("type", "button");
+        b.setAttribute("id", "done");
+        b.setAttribute("value", "Done");
+        b.setAttribute("onclick", "doneTask(this)");
+
+        var c = document.createElement("input");
+        c.setAttribute("type", "button");
+        c.setAttribute("id", "delete");
+        c.setAttribute("value", "Delete");
+        c.setAttribute("onclick", "deleteTask(this);");
+
+        var d = document.createElement("li");
+        d.appendChild(a);
+        d.appendChild(b);
+        d.appendChild(c);
+        
+        var e = document.getElementById("cont");
+        e.appendChild(d);
+
+        element.value = "";
+
+    }
+}
+
+function doneTask(este) {
+
+    var a = este.parentNode;
+    var b = a.firstChild;
+    var task = b.innerHTML;
+    var newElem = document.createElement("s");
+
+    newElem.textContent = task;
+
+    console.log(newElem);
+
+    a.replaceChild(newElem, b);
+
+    este.remove();
+
+}
+
+function deleteTask(este) {
+
+    este.parentNode.remove();
+
+} 
